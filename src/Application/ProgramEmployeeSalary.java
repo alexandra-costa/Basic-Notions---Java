6c068b5361d1a259ac58a9b4e8baa6f4e65598c8
alexandra-costa <alexandraaraujodacosta@gmail.com>
2021-08-03 21:22:17 -0300

    Exercise Java OO Part2

--- src/Application/ProgramEmployeeSalary.java
+++ src/Application/ProgramEmployeeSalary.java
@@ -7,6 +7,32 @@ public class ProgramEmployeeSalary {
     public static void main (String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Employee emp = new Employee();
+
+        System.out.print("Name: \n");
+        emp.name = sc.nextLine();
+
+        System.out.println();
+        System.out.print("GrossSalary: \n");
+        emp.grossSalary = sc.nextDouble();
+
+        System.out.println();
+        System.out.print("Tax: \n");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        double netSalary = emp.netSalary();
+        System.out.println("Employee: " + emp);
+
+        System.out.println();
+        System.out.print("Wich percentage to increase salary?");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Update data: " + emp);
+
+
 
 
 
