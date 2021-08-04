29665849fcf90e5e7eff21449c3b5f913ea98716
alexandra-costa <alexandraaraujodacosta@gmail.com>
2021-08-04 19:08:25 -0300

    fix: code refactoration Product and ProgramStock

--- src/Application/ProgramStock.java
+++ src/Application/ProgramStock.java
@@ -3,29 +3,29 @@ import java.util.Locale;
 import entities.Product;
 import java.util.Scanner;
 public class ProgramStock {
-    public static void main (String [] args){
+    public static void main (String [] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-        System.out.println("Enter product data: ");
 
+        System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
 
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
 
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
 
+        Product product = new Product(name, price, quantity); // construtor
 
         System.out.println();
         System.out.println("Product data: " + product);
 
         System.out.println();
         System.out.println("Enter the number of products to be added in stock: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
@@ -39,8 +39,7 @@ public class ProgramStock {
         System.out.println();
         System.out.println("Updated data: " + product);
 
-
-
          sc.close();
     }
+
 }
